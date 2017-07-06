a22a145e0c752291364c01958e0a2e1b745ec6ef
zhengdakang <1577886278@qq.com>
2017-07-06 13:34:22 +0800

    App.vue header and footer

--- 004_Vue/VueLearn/src/App.vue
+++ 004_Vue/VueLearn/src/App.vue
@@ -3,11 +3,29 @@
 <!-- template 主要存放html元素的 -->
 <template>
     <div>
-        <router-link to="/login">登录</router-link>
-        <router-link to="/register">注册</router-link>
-
+        <!-- 1.0 利用mint-ui中的header组件实现整个系统的头部 -->
+        <mt-header fixed title="Vue商城"></mt-header>
         <!-- 路由占位符 -->
         <router-view></router-view>
+        <!-- 3.0 利用mui中的tabbar组件实现系统的底部 -->
+        <nav class="mui-bar mui-bar-tab">
+			<a class="mui-tab-item mui-active" href="#tabbar">
+				<span class="mui-icon mui-icon-home"></span>
+				<span class="mui-tab-label">首页</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-chat">
+				<span class="mui-icon mui-icon-email"></span>
+				<span class="mui-tab-label">会员</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-contact">
+				<span class="mui-icon mui-icon-contact"><span class="mui-badge">9</span></span>
+				<span class="mui-tab-label">购物车</span>
+			</a>
+			<a class="mui-tab-item" href="#tabbar-with-map">
+				<span class="mui-icon mui-icon-gear"></span>
+				<span class="mui-tab-label">搜索</span>
+			</a>
+		</nav>
     </div>
 </template>
 
--- 004_Vue/VueLearn/src/main.js
+++ 004_Vue/VueLearn/src/main.js
@@ -2,10 +2,22 @@
 import Vue from "vue";
 
 // 2.0 导入App.vue的vue对象
-import App from './App.vue';
+import App from "./App.vue";
+
+// 5.0 导入mint-ui的css文件
+import "mint-ui/lib/style.min.css";
+
+// 5.1 导入mint-ui组件对象
+import mintui from "mint-ui";
+
+// 5.2 全局中使用mint-ui
+Vue.use(mintui);
+
+// 6.0 注册mui的css文件
+import "../statics/mui/css/mui.css";
 
 // 4.0 导入vue-router
-import vueRouter from 'vue-router';
+import vueRouter from "vue-router";
 
 // 4.1 将vueRouter绑定到vue对象上
 Vue.use(vueRouter);
