02dca1ae77b4a1a9f1c7c9f5e185c717e7ec6bab
zhengdakang <1577886278@qq.com>
2017-07-11 11:25:24 +0800

    返回按钮功能

--- VueLearn/src/App.vue
+++ VueLearn/src/App.vue
@@ -7,27 +7,32 @@
         <mt-header fixed title="Vue商城"></mt-header>
         <!-- 路由占位符 -->
         <router-view></router-view>
+
+        <div v-if="isshow" class="back">
+            <a href="javascript:void(0)" @click="backto">返回</a>
+        </div>
+
         <!-- 3.0 利用mui中的tabbar组件实现系统的底部 -->
         <nav class="mui-bar mui-bar-tab">
-			<router-link class="mui-tab-item" to="/home">
-				<span class="mui-icon mui-icon-home"></span>
-				<span class="mui-tab-label">首页</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/tabbar-with-chat">
-				<span class="mui-icon mui-icon-email"></span>
-				<span class="mui-tab-label">会员</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/shopcar/car">
-				<span class="mui-icon mui-icon-contact">
+            <router-link class="mui-tab-item" to="/home">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"></span>
+                <span class="mui-tab-label">会员</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/shopcar/car">
+                <span class="mui-icon mui-icon-contact">
                     <span  id="badge" class="mui-badge">0</span>
                 </span>
-				<span class="mui-tab-label">购物车</span>
-			</router-link>
-			<router-link class="mui-tab-item" to="/tabbar-with-map">
-				<span class="mui-icon mui-icon-gear"></span>
-				<span class="mui-tab-label">搜索</span>
-			</router-link>
-		</nav>
+                <span class="mui-tab-label">购物车</span>
+            </router-link>
+            <router-link class="mui-tab-item" to="/tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">搜索</span>
+            </router-link>
+        </nav>
     </div>
 </template>
 
@@ -36,12 +41,26 @@
 import {vm, COUNTSTR} from "./kits/vm.js";
 vm.$on(COUNTSTR,function(count){
     var badgeobj = document.querySelector('#badge');
-	badgeobj.innerText = parseInt(badgeobj.innerText) + count;
+    badgeobj.innerText = parseInt(badgeobj.innerText) + count;
 })
 export default{
     data(){
         return {
-            msg : "Hello"
+            isshow : false
+        }
+    },
+    watch : {
+        "$route" : function(newroute, oldroute){
+            if (newroute.path.toLowerCase() == "/home") {
+                this.isshow = false;
+            }else {
+                this.isshow = true;
+            }
+        }
+    },
+    methods : {
+        backto : function(){
+            this.$router.go(-1)
         }
     }
 }
@@ -50,5 +69,17 @@ export default{
 <style scope>
     .red{
         color : red
+    }
+    .back{
+        width: 60px;
+        position: absolute;
+        top:10px;
+        left:10px;
+        z-index: 101;
+    }
+    .back a{
+        color:white;
+        font-size: 16px;
+        font-weight: bold;
     }
 </style>
